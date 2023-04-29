2d167843993cadcedb78dbae3f3435a072d0a363
Fatih Şen <fatihydrm@outlook.com>
2023-04-29 20:33:03 +0300

    edit: redesign header component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,34 +1,53 @@
 <template>
-   <nav>
+   <nav :class="moviePage ? 'movie-page' : null">
       <div class="container">
          <h1><NuxtLink to="/">Nuxt Movie</NuxtLink></h1>
-
-         <ul class="lists">
-            <li>
-               <NuxtLink to="/movies">Movies</NuxtLink>
-            </li>
-         </ul>
          <SearchBox />
       </div>
    </nav>
 </template>
 
+<script setup lang="ts">
+let moviePage = ref<string | string[]>();
+const route = useRoute();
+moviePage.value = route.params.movieid;
+
+watch(route, (newVal: any, oldVal: any) => {
+   console.log(route.params);
+   moviePage.value = route.params.movieid;
+});
+</script>
+
 <style lang="scss">
 nav {
-   background-color: #00dc82;
-   color: #222;
+   background-color: transparent;
+   position: sticky;
+   width: 100%;
+   top: 0;
+   z-index: 99;
+   height: 80px;
+   display: flex;
+   align-items: center;
+
+   &.movie-page {
+      background: rgba(4, 167, 119, 0.16);
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.4);
+      backdrop-filter: blur(3.5px);
+      -webkit-backdrop-filter: blur(3.5px);
+   }
 
    & > .container {
       padding: 10px 20px;
       display: flex;
       align-items: center;
+      justify-content: space-between;
 
       a {
-         color: #222;
+         color: #fff;
       }
 
       & > h1 {
-         font-weight: 700;
+         font-weight: 800;
          font-size: 28px;
       }
       .lists {
@@ -41,6 +60,15 @@ nav {
          & > li {
             font-weight: 700;
          }
+      }
+   }
+}
+@media (width <= 705px) {
+   nav {
+      height: auto;
+      .container {
+         flex-direction: column;
+         gap: 10px;
       }
    }
 }
